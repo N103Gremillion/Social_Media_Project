7169625d84b25bff1bad3924dafa2292d8589797
N103Gremillion <nathan103grem@gmail.com>
2024-10-05 15:50:38 -0500

    toolbar looks much better /added css to elements of toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,34 +2,51 @@ import React,{ useState } from "react"
 
 
 const Toolbar = () => {
-  const [curButton, setCurButton] = useState(null);
-  const [tag, setTag] = useState('Nathan');
-  
-  // function to handle button clicks
-  const handleButtonClick = (buttonId) => {
-    setCurButton(buttonId);
-    console.log('Button ${buttonId} clicked!');
-  }
 
-  // styling for the buttons
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  console.log('Window width: ${width}, Window height: ${height}px');
+  
+  const divStyle = {
+    backgroundColor: 'skyblue',
+    width: '5%',
+    height: '100vh',
+    padding: '20px',
+    borderRadius: '10px',
+    textAlign: 'center',
+    // make the items in div vertially align
+    display: 'flex',
+    alignItems:'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'column',
+    border: '2px solid royalblue',
+  };
+
   const buttonStyle = {
-    margin: '1 auto',
-    color: 'black',
-    padding: '0.6rem 1.2rem',
-    border: 'none',
-  }
+    backgroundColor: 'lightblue',
+    color: 'blue',
+    // flex set to 1 so each button takes upt equal width
+    width: '200%',
+    height: '10%',
+    border: '2px solid blue',
+    padding: '10px 10px',
+    borderRadius: '10px',
+    textAlign: 'center',
+    fontSize: '16px',
+    // changes the currsor to pointer when hovering
+    cursor: 'pointer',
+  };
 
   return (
-    <div className="toolbar">
-      <p>Toolbar</p>
-        <div className="buttonContainer" style={{textAlign: 'left', marginTop: '2rem' }}>
-            <button style={buttonStyle} onClick={() => handleButtonClick(1)}>Button 1</button>
-            <button style={buttonStyle} onClick={() => handleButtonClick(2)}>Button 2</button>
-            <button style={buttonStyle} onClick={() => handleButtonClick(3)}>Button 3</button>
-            <button style={buttonStyle} onClick={() => handleButtonClick(4)}>Button 4</button>
-        </div>
+    <div style={divStyle}>
+      <button style={buttonStyle}>Test</button>
+      <button style={buttonStyle}>Test</button>
+      <button style={buttonStyle}>Test</button>
+      <button style={buttonStyle}>Test</button>
+      <button style={buttonStyle}>Test</button>
     </div>
-  )
+  );
 }
 
 export default Toolbar

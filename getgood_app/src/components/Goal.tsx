4992f437237b0640a518dbe107c4f9375afdbcdf
Lucas-Alvarado <lucasalvarado615@gmail.com>
2024-10-06 20:42:55 -0500

    Started goal component

---
+++ getgood_app/src/components/Goal.tsx
@@ -0,0 +1,17 @@
+
+function Goal() {
+    return (
+        <div className="card">
+  <div className="card-header">
+    Goal
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Some descriptor of the goal.</h5>
+    <p className="card-text">Even more descriptions of goal.</p>
+    <a href="#" className="btn btn-primary">Do something</a>
+  </div>
+</div>
+    ); 
+}
+
+export default Goal; 

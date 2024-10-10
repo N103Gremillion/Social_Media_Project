e669f849fe6ff9b2f8928256ada3aa65c2c034fb
Lucas-Alvarado <lucasalvarado615@gmail.com>
2024-10-09 19:31:50 -0500

    Integrated router to App.tsx

--- getgood_app/src/App.tsx
+++ getgood_app/src/App.tsx
@@ -1,10 +1,33 @@
-import MyGoalsPage from './pages/MyGoalsPage'
+import React from "react";
+import MyGoalsPage from "./pages/MyGoalsPage";
+import Toolbar from "./components/Toolbar";
+import ErrorPage from "./pages/ErrorPage";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Toolbar />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "MyGoalsPage", element: <MyGoalsPage /> }],
+  },
+]);
 
 function App() {
   return (
-  <div>
-    <MyGoalsPage /> 
-  </div>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
+
 export default App;
---
+++ getgood_app/src/pages/MyGoalsPage.tsx
@@ -0,0 +1,13 @@
+import Goal from "../components/Goal";
+
+function MyGoalsPage() {
+  return (
+    <div>
+      <Goal />
+      <Goal />
+      <Goal />
+    </div>
+  );
+}
+
+export default MyGoalsPage;

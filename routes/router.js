fa63166d5dd2e9c82a10fdaa2a5f6012537b7969
N103Gremillion <nathan103grem@gmail.com>
2024-10-17 00:41:23 -0500

    added all get/put requests in a router

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const mysql = require('mysql2');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'profilePictures/');
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + path.extname(file.originalname));
+	}
+});
+
+const upload = multer({ storage: storage });
+
+const pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: 'BarOfChocolateEarPhones',
+	database: 'csc403'
+});
 
 router.get('/getProfilePicture', (req, res) => {
   const userId = req.query.userId;
@@ -22,5 +42,140 @@ router.get('/getProfilePicture', (req, res) => {
         res.sendFile(path.resolve(__dirname, imagePath));
     });
 })
+
+router.get('/getUserGoals', (req,res) => {
+	const  userId  = req.query.userId;
+
+	const getUserGoalsQuery = 'select goals.name, goals.description, goals.start_date, goals.end_date, goals.current_progress from goals join user_goals on goals.id = user_goals.goal_id where user_goals.user_id = ?';
+
+	pool.query(getUserGoalsQuery, [userId], (error,results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message});
+		}
+		res.json(results);
+	});
+});
+
+router.post('/addUser', (req,res) => {
+	const { name, email, password } = req.body;
+
+	const addUserCommand = 'insert into users (name,email,password) values (?,?,?)';
+
+	pool.query(addUserCommand, [name, email, password], (error,results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message });
+		}
+		res.status(201).json({ id: results.insertId });
+	});
+});
+
+router.post('/checkForUser', (req,res) => {
+	const { userName, userPassword } = req.body;
+
+	const checkForUserCommand = 'select id from users where name = ? and password = ?';
+
+	pool.query(checkForUserCommand, [userName, userPassword], (error, results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message });
+		}
+		res.status(201).json({ results });
+	})
+})
+
+router.post('/changeUserName', (req,res) => {
+	const {userId, newName} = req.body;
+
+	const changeNameQuery = 'update users set name = ? where id = ?';
+
+	pool.query(changeNameQuery, [newName, userId], (error,results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message });
+		}
+		res.status(201).json({ success: true });
+	});
+});
+
+router.post('/changeUserPassword', (req,res) => {
+	const {userId, newPassword} = req.body;
+
+	const changeNameQuery = 'update users set password = ? where id = ?';
+
+	pool.query(changeNameQuery, [newPassword, userId], (error,results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message });
+		}
+		res.status(201).json({ success: true });
+	});
+});
+
+router.post('/addGoal', (req,res) => {
+	const { userId, goalName, goalDescription, goalStartDate, goalEndDate } = req.body;
+
+	const addGoalUserConnectionQuery = 'insert into user_goals (user_id,goal_id) values (?,?)';
+	const addGoalCommand = 'insert into goals (name,description,start_date,end_date) values (?,?,?,?)';
+
+	pool.query(addGoalCommand, [goalName, goalDescription,goalStartDate,goalEndDate], (error,results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message });
+		}
+		addGoalUserConnection(userId,results.insertId,addGoalUserConnectionQuery,req,res);
+	});
+});
+
+router.post('/deleteGoal', (req,res) => {
+	const { userId, goalId } = req.body;
+
+	const deleteQuery = "delete from goals where id=?";
+	const deleteConnectionQuery = "delete from user_goals where user_id=? and goal_id=?";
+	pool.query(deleteQuery, [goalId], (error,results) => {
+		if (error) {
+			return res.status(500).json({error: error.message});
+		}
+	});
+
+	pool.query(deleteConnectionQuery, [userId, goalId], (error, results) => {
+		if (error) {
+			return res.status(500).json({ error: error.message });
+		}
+		res.status(201).json({ deleted: true });
+	});
+});
+
+router.post('/addCheckpoint', (req,res) => {
+	const { goalId, name, date} = req.body;
+	const addQuery = "insert into checkpoints (name, date) values (?,?)";
+	const addConnectionQuery = "insert into goal_checkpoints (goal_id, checkpoint_id) values (?,?)";
+
+	pool.query(addQuery, [name, date],(error, results) => {
+                if (error) {
+                        return res.status(500).json({ error: error.message });
+                }
+		addGoalCheckpointConnection(results.insertId,goalId,addConnectionQuery,req,res);
+        });
+});
+
+router.post('/incrementLikes', (req,res) => {
+	const { goalId } = req.body;
+
+	const incrementLikesQuery = "update goals set likes = likes + 1 where id = ?";
+	pool.query(incrementLikesQuery, [goalId], (error,results) => {
+		if (error) {
+			return res.status(500).json({error: error.message});
+		}
+	res.status(201).json({ message: "Likes incremented" });
+	});
+});
+
+router.post('/decrementLikes', (req,res) => {
+	const { goalId } = req.body;
+
+	const incrementLikesQuery = "update goals set likes = likes - 1 where id = ?";
+	pool.query(incrementLikesQuery, [goalId], (error,results) => {
+		if (error) {
+			return res.status(500).json({error: error.message});
+		}
+	res.status(201).json({ message: "Likes decremented" });
+	});
+});
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mysql = require('mysql2');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
@@ -8,8 +7,9 @@ const router = require('./routes/router');
 
 const app = express();
 
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 const corsOptions = {
 	origin: '*',
@@ -22,24 +22,6 @@ app.use('/', router);
 
 const port = 4000;
 
-
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'profilePictures/');
-	},
-	filename: (req, file, cb) => {
-		cb(null, Date.now() + path.extname(file.originalname));
-	}
-});
-
-const upload = multer({ storage: storage });
-
-const pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'BarOfChocolateEarPhones',
-	database: 'csc403'
-});
 
 // app.get('/getProfilePicture', (req,res) => {
 // 	const userId = req.query.userId;
@@ -57,142 +39,141 @@ const pool = mysql.createPool({
 // 	});
 // });
 
-app.get('/getUserGoals', (req,res) => {
-	const  userId  = req.query.userId;
+// app.get('/getUserGoals', (req,res) => {
+// 	const  userId  = req.query.userId;
 
-	const getUserGoalsQuery = 'select goals.name, goals.description, goals.start_date, goals.end_date, goals.current_progress from goals join user_goals on goals.id = user_goals.goal_id where user_goals.user_id = ?';
+// 	const getUserGoalsQuery = 'select goals.name, goals.description, goals.start_date, goals.end_date, goals.current_progress from goals join user_goals on goals.id = user_goals.goal_id where user_goals.user_id = ?';
 
-	pool.query(getUserGoalsQuery, [userId], (error,results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message});
-		}
-		res.json(results);
-	});
-});
+// 	pool.query(getUserGoalsQuery, [userId], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message});
+// 		}
+// 		res.json(results);
+// 	});
+// });
 
 
-app.post('/addUser', (req,res) => {
-	const { name, email, password } = req.body;
+// app.post('/addUser', (req,res) => {
+// 	const { name, email, password } = req.body;
 
-	const addUserCommand = 'insert into users (name,email,password) values (?,?,?)';
+// 	const addUserCommand = 'insert into users (name,email,password) values (?,?,?)';
 
-	pool.query(addUserCommand, [name, email, password], (error,results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message });
-		}
-		res.status(201).json({ id: results.insertId });
-	});
-});
+// 	pool.query(addUserCommand, [name, email, password], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message });
+// 		}
+// 		res.status(201).json({ id: results.insertId });
+// 	});
+// });
 
-app.post('/checkForUser', (req,res) => {
-	const { userName, userPassword } = req.body;
+// app.post('/checkForUser', (req,res) => {
+// 	const { userName, userPassword } = req.body;
 
-	const checkForUserCommand = 'select id from users where name = ? and password = ?';
+// 	const checkForUserCommand = 'select id from users where name = ? and password = ?';
 
-	pool.query(checkForUserCommand, [userName, userPassword], (error, results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message });
-		}
-		res.status(201).json({ results });
-	})
-})
+// 	pool.query(checkForUserCommand, [userName, userPassword], (error, results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message });
+// 		}
+// 		res.status(201).json({ results });
+// 	})
+// })
 
-app.post('/changeUserName', (req,res) => {
-	const {userId, newName} = req.body;
+// app.post('/changeUserName', (req,res) => {
+// 	const {userId, newName} = req.body;
 
-	const changeNameQuery = 'update users set name = ? where id = ?';
+// 	const changeNameQuery = 'update users set name = ? where id = ?';
 
-	pool.query(changeNameQuery, [newName, userId], (error,results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message });
-		}
-		res.status(201).json({ success: true });
-	});
-});
+// 	pool.query(changeNameQuery, [newName, userId], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message });
+// 		}
+// 		res.status(201).json({ success: true });
+// 	});
+// });
 
-app.post('/changeUserPassword', (req,res) => {
-	const {userId, newPassword} = req.body;
+// app.post('/changeUserPassword', (req,res) => {
+// 	const {userId, newPassword} = req.body;
 
-	const changeNameQuery = 'update users set password = ? where id = ?';
+// 	const changeNameQuery = 'update users set password = ? where id = ?';
 
-	pool.query(changeNameQuery, [newPassword, userId], (error,results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message });
-		}
-		res.status(201).json({ success: true });
-	});
-});
+// 	pool.query(changeNameQuery, [newPassword, userId], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message });
+// 		}
+// 		res.status(201).json({ success: true });
+// 	});
+// });
 
-app.post('/addGoal', (req,res) => {
-	const { userId, goalName, goalDescription, goalStartDate, goalEndDate } = req.body;
+// app.post('/addGoal', (req,res) => {
+// 	const { userId, goalName, goalDescription, goalStartDate, goalEndDate } = req.body;
 
-	const addGoalUserConnectionQuery = 'insert into user_goals (user_id,goal_id) values (?,?)';
-	const addGoalCommand = 'insert into goals (name,description,start_date,end_date) values (?,?,?,?)';
+// 	const addGoalUserConnectionQuery = 'insert into user_goals (user_id,goal_id) values (?,?)';
+// 	const addGoalCommand = 'insert into goals (name,description,start_date,end_date) values (?,?,?,?)';
 
-	pool.query(addGoalCommand, [goalName, goalDescription,goalStartDate,goalEndDate], (error,results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message });
-		}
-		addGoalUserConnection(userId,results.insertId,addGoalUserConnectionQuery,req,res);
-	});
-});
+// 	pool.query(addGoalCommand, [goalName, goalDescription,goalStartDate,goalEndDate], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message });
+// 		}
+// 		addGoalUserConnection(userId,results.insertId,addGoalUserConnectionQuery,req,res);
+// 	});
+// });
 
-app.post('/deleteGoal', (req,res) => {
-	const { userId, goalId } = req.body;
+// app.post('/deleteGoal', (req,res) => {
+// 	const { userId, goalId } = req.body;
 
-	const deleteQuery = "delete from goals where id=?";
-	const deleteConnectionQuery = "delete from user_goals where user_id=? and goal_id=?";
-	pool.query(deleteQuery, [goalId], (error,results) => {
-		if (error) {
-			return res.status(500).json({error: error.message});
-		}
-	});
+// 	const deleteQuery = "delete from goals where id=?";
+// 	const deleteConnectionQuery = "delete from user_goals where user_id=? and goal_id=?";
+// 	pool.query(deleteQuery, [goalId], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({error: error.message});
+// 		}
+// 	});
 
-	pool.query(deleteConnectionQuery, [userId, goalId], (error, results) => {
-		if (error) {
-			return res.status(500).json({ error: error.message });
-		}
-		res.status(201).json({ deleted: true });
-	});
-});
+// 	pool.query(deleteConnectionQuery, [userId, goalId], (error, results) => {
+// 		if (error) {
+// 			return res.status(500).json({ error: error.message });
+// 		}
+// 		res.status(201).json({ deleted: true });
+// 	});
+// });
 
-app.post('/addCheckpoint', (req,res) => {
-	const { goalId, name, date} = req.body;
-	const addQuery = "insert into checkpoints (name, date) values (?,?)";
-	const addConnectionQuery = "insert into goal_checkpoints (goal_id, checkpoint_id) values (?,?)";
+// app.post('/addCheckpoint', (req,res) => {
+// 	const { goalId, name, date} = req.body;
+// 	const addQuery = "insert into checkpoints (name, date) values (?,?)";
+// 	const addConnectionQuery = "insert into goal_checkpoints (goal_id, checkpoint_id) values (?,?)";
 
-	pool.query(addQuery, [name, date],(error, results) => {
-                if (error) {
-                        return res.status(500).json({ error: error.message });
-                }
-		addGoalCheckpointConnection(results.insertId,goalId,addConnectionQuery,req,res);
-        });
-});
+// 	pool.query(addQuery, [name, date],(error, results) => {
+//                 if (error) {
+//                         return res.status(500).json({ error: error.message });
+//                 }
+// 		addGoalCheckpointConnection(results.insertId,goalId,addConnectionQuery,req,res);
+//         });
+// });
 
-app.post('/incrementLikes', (req,res) => {
-	const { goalId } = req.body;
+// app.post('/incrementLikes', (req,res) => {
+// 	const { goalId } = req.body;
 
-	const incrementLikesQuery = "update goals set likes = likes + 1 where id = ?";
-	pool.query(incrementLikesQuery, [goalId], (error,results) => {
-		if (error) {
-			return res.status(500).json({error: error.message});
-		}
-	res.status(201).json({ message: "Likes incremented" });
-	});
-});
+// 	const incrementLikesQuery = "update goals set likes = likes + 1 where id = ?";
+// 	pool.query(incrementLikesQuery, [goalId], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({error: error.message});
+// 		}
+// 	res.status(201).json({ message: "Likes incremented" });
+// 	});
+// });
 
-app.post('/decrementLikes', (req,res) => {
-	const { goalId } = req.body;
+// app.post('/decrementLikes', (req,res) => {
+// 	const { goalId } = req.body;
 
-	const incrementLikesQuery = "update goals set likes = likes - 1 where id = ?";
-	pool.query(incrementLikesQuery, [goalId], (error,results) => {
-		if (error) {
-			return res.status(500).json({error: error.message});
-		}
-	res.status(201).json({ message: "Likes decremented" });
-	});
-});
-
+// 	const incrementLikesQuery = "update goals set likes = likes - 1 where id = ?";
+// 	pool.query(incrementLikesQuery, [goalId], (error,results) => {
+// 		if (error) {
+// 			return res.status(500).json({error: error.message});
+// 		}
+// 	res.status(201).json({ message: "Likes decremented" });
+// 	});
+// });
 
 function addGoalUserConnection(userId,goalId,query,req,res) {
 	pool.query(query, [userId, goalId], (error, results) => {
@@ -214,5 +195,6 @@ function addGoalCheckpointConnection(checkpointId,goalId,query,req,res) {
 
 
 app.listen(port, () => {
-	console.log('server is running on port ${port}');
+	console.log('server is running on port ' , port);
 });
+

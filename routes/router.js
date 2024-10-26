518ead566ca08e6878c025ef83e78507f2f28ea3
N103Gremillion <nathan103grem@gmail.com>
2024-10-26 17:38:53 -0500

    small change I forgot

--- routes/router.js
+++ routes/router.js
@@ -53,7 +53,7 @@ const upload = multer({
 const pool = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
-	password: 'BarOfChocolateEarPhones',
+	password: 'Chi538Burger',
 	database: 'csc403'
 });
 

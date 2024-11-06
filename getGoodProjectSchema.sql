3cc345daece19df8a46d331067598461cd49ee4d
N103Gremillion <nathan103grem@gmail.com>
2024-11-05 21:10:57 -0600

    got profile pictures images working

--- getGoodProjectSchema.sql
+++ getGoodProjectSchema.sql
@@ -6,7 +6,8 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL,
+    profilePicture VARCHAR(255) DEFAULT './defaultProfile.jpg'
 );
 
 CREATE TABLE goals (

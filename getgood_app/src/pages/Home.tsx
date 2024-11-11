11c0df55b213c1bfb8695890215c221f237ed59d
N103Gremillion <nathan103grem@gmail.com>
2024-11-11 01:15:49 -0600

    started on home page and got a better layout

--- getgood_app/src/pages/Home.tsx
+++ getgood_app/src/pages/Home.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "../components/styles/home.css"
+import { useState, useEffect } from "react";
+
+interface users {
+  userId: number,
+  name: string,
+  profilePicURL: string
+}
+
+
 
 const Home = () => {
+
+  const [followersInfo, setFollowersInfo] = useState<users[]>([]);
+  const [followerIds, setFollowerIds] = useState<number[]>([]);
+  const BASE_URL: string = 'http://localhost:4000/';
+  const id = sessionStorage.getItem('userID');
+
+
   return (
     <div className="home-page">
-
+      <div className="followers-scroller">
+        {/* all the users you follow */}
+        {followersInfo.map((user) => (
+          <span> key={user.userId}
+            <p>user.name</p>
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
